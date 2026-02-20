5aaafbe8a0c14274f675e3b553cece69bee3d107
AbrahamGlzP <abrahamglzp@gmail.com>
2026-02-20 14:49:49 -0600

    Initial Commit

---
+++ MainThreadViolationsDemo/MainThreadViolationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainThreadViolationsDemoApp.swift
+//  MainThreadViolationsDemo
+//
+//  Created by Abraham Gonzalez Puga on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MainThreadViolationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
